4ef4b3dddea4ed271fd2df3007360bbeee3e29c6
nrjsharma <nrjsharma083@gmail.com>
2020-06-01 02:27:14 +0530

    update life-cycle

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,7 +3,33 @@ import '../../containers/App.css';
 import Person from './Person/Person'
 
 class Persons extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log('[Persons.js] constructor')
+    }
+
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps')
+    //     return state
+    // }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate')
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate')
+        return {message: "Ja mata di"}
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate', snapshot)
+    }
+
     render() {
+        console.log('[Persons.js] render')
         return this.props.persons.map((person, index) => {
             return (
                 <Person
